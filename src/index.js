0fc5d2334609e0c610e2a01999a35fc2d9c6c00f
Pranav Munjal <36812178+FizzyPM@users.noreply.github.com>
2018-09-27 04:44:06 +0530

    Delete Todo
    Deletion from todos list

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-
+let id = 0;
 const Todo = props => (
 	<li>
 		<input type="checkbox" />
-		<button>delete</button>
+		<button onClick={props.onDelete}>delete</button>
 		<span>{props.todo.text}</span>
 	</li>
 );
@@ -20,13 +20,20 @@ class App extends React.Component {
 	}
 	addTodo() {
 		const text = prompt("TODO text please!");
-		this.setState({ todos: [...this.state.todos, { text: text }] });
+		this.setState({ todos: [...this.state.todos, { id: id++, text: text }] });
+	}
+	deleteTodo(id) {
+		this.setState({ todos: this.state.todos.filter(todo => todo.id !== id) });
 	}
 	render() {
 		return (
 			<div>
 				<button onClick={() => this.addTodo()}>Add Todo</button>
-				<ul>{this.state.todos.map(todo => <Todo todo={todo} />)}</ul>
+				<ul>
+					{this.state.todos.map(todo => (
+						<Todo todo={todo} onDelete={() => this.deleteTodo(todo.id)} />
+					))}
+				</ul>
 			</div>
 		);
 	}
